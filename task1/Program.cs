c16b0e024376e990a7f27588040a0a0a4a3a27ce
Anna <ds_anna@bk.ru>
2022-06-20 19:53:50 +0300

    Найти сумму цифр числа

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму цифр числа
+int RecSum(int num)
+{
+    if (num == 0)
+        return num;
+    return num % 10 + RecSum(num / 10);
+}
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма цифр числа = {RecSum(Math.Abs(num))}");
+
+// Через тернарный оператор:
+
+// uint SumDigits(uint n) => n<10 ? n : n%10 + SumDigits(n/10);
+// Console.Write("Enter your number: ");
+// uint n = uint.Parse(Console.ReadLine() ?? "0");
+// Console.WriteLine($"Sum of digits {SumDigits(n)}");
